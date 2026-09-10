ccb5c24c6b73f3707e7654ade7297aab2ecea992
kolya kovalyov <elitevarvar696969@gmail.com>
2026-09-10 12:00:53 +0300

    “commitOne”

---
+++ Zadachi/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[] graf = ReadArray(Console.ReadLine());
+            
+            Node root = new Node();
+            foreach (var g in graf)
+            {
+                
+            }
+        }
+    }
+    public static int[] ReadArray(string s)
+    {
+        var s2 = s.Split(' ');
+        int[] res = new int[s2.Length];
+        for (int i = 0; i < res.Length; i++)
+        {
+            res[i] = Convert.ToInt32(s2[i]);
+        }
+        return res;
+    }
+    public class Node
+    {
+        public int num;
+        public List<Node> kids = new List<Node>();
+        public Node this[int i]
+        {
+            get { return kids.Find(x => x.num == i); }
+            set {  }
+        }
+    }
+}
